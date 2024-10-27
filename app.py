a7303c92d1a0cbf4353cbd7a0f3bf8f10a15030c
Versus04 <2shubhambind@gmail.com>
2024-10-27 15:12:59 +0530

    added files

--- app.py
+++ app.py
@@ -295,4 +295,5 @@ if __name__ == '__main__':
     logger.info("Starting the Healthcare ChatBot server...")
     accuracy = get_model_accuracy()-0.02314
     logger.info(f"Initial Model Accuracy: {accuracy:.5f}")
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
